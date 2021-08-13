4b0a3a98a0df4a7e99a7e7c32a8ebbfed23e3527
Hemant Gauhai <77909457+itsmehemantgauhai01@users.noreply.github.com>
2021-08-13 11:18:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let myLeads=[]
+const ulEl=document.getElementById("ul-el")
+const inputEl= document.getElementById("#input-el")
+const inputBtn= document.getElementById("#input-btn")
+
+// function saveInput(){
+//     inputEl.textContent=inputEl.value
+//     inputBtn.textContent=myLeads.push(inputEl.value)
+//     ulEl.textContent=myLeads[i]
+// }
+
+inputBtn.addEventListener("click",function(){
+    inputEl.value=""
+    myLeads.push(inputEl.value)
+    renderLeads()
+})
+
+function renderLeads(){
+    let listItem=""
+    for(let i=0;i<myLeads.length;i++){
+        //listItem += "<li><a target='_blank' href='"+myLeads[i]+"'>"+myLeads[i]+"</a></li>"
+        listItem += `<li>
+                        <a target='_blank' href='${myLeads[i]}'>
+                        ${myLeads[i]}
+                        </a>
+                    </li>`
+        // const li=document.createElement("li")
+        // li.textContent=myLeads[i]
+        // ulEl.append(li)
+    }
+    ueEl.innerHTML = listItem
+}
